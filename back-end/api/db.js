0655c9ba096b06c5b19b98047420f0d72471e51d
mehedimrm22 <mehedi.mrm@gmail.com>
2025-01-08 22:40:32 +0200

    json server updated

---
+++ back-end/api/db.js
@@ -0,0 +1,13 @@
+import jsonServer from 'json-server';
+import path from 'path';
+
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(process.cwd(), 'db.json'));
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+
+export default (req, res) => {
+    server.emit('request', req, res);
+};
